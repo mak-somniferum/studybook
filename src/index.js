42bb6c86982b575a89a75c827020b96705fa7731
mak-somniferum <suhjin700@gmail.com>
2022-11-14 17:24:29 +0900

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Library from './chapter_03/Library';
-import Clock from './chapter_04/Clock';
-import CommentList from './chapter_05/CommentList';
-import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
