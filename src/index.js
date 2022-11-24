859f8a11e2bc1b72fa2ec542b48f85202bba8c91
mak-somniferum <suhjin700@gmail.com>
2022-11-24 11:09:02 +0900

    리스트와 키

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <LandingPage />
+    <AttendanceBook />
   </>
 );
 
