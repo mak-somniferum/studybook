89ff8f20ddf547f0270e0b7cd1e4b127cca9f82e
mak-somniferum <suhjin700@gmail.com>
2022-12-06 08:59:01 +0900

    .

--- src/chapter_14/DarkOrLight.jsx
+++ src/chapter_14/DarkOrLight.jsx
@@ -4,14 +4,14 @@ import ThemeContext from "./ThemeContext";
 
 function DarkOrLight(props) {
   const [theme, setTheme] = useState("light");
-  
+
   const toggleTheme = useCallback(() => {
-    if(theme == "light") {
+    if(theme === "light") {
       setTheme("dark");
-    }else if(theme == "dark") {
+    }else if(theme === "dark") {
       setTheme("light");
     }
-  });
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
